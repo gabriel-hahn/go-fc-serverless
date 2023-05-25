fe32a37edae1b16f9b6907c7cfac170805140449
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-05-25 20:22:59 -0300

    feat: add listProducts lambda

---
+++ src/lambdas/listProducts/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"go-fc-serverless/src/services"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(services.ListProducts)
+}
---
+++ src/services/listProducts.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"go-fc-serverless/src/data"
+	"go-fc-serverless/src/database"
+
+	"encoding/json"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func ListProducts(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	result, err := database.GetAll("ProductsVideo")
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	var products []data.Product
+	for _, item := range result.Items {
+		price, err := strconv.Atoi(*item["price"].N)
+		if err != nil {
+			return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+		}
+
+		products = append(products, data.Product{
+			ID: *item["id"].S,
+			Name: *item["name"].S,
+			Price: price,
+		})
+	}
+
+	body, err := json.Marshal(products)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string {
+			"Content-type": "application/json",
+		},
+		Body: string(body),
+	}, nil
+}
